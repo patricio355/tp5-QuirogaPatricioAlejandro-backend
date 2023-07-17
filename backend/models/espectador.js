8d4852eee79aa89d97c3b7651793b8013496c8b5
patricio355 <patricioquiroga35@gmail.com>
2023-07-16 21:31:57 -0300

    borro backend

--- backend/models/espectador.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-const EspectadorSchema = new Schema({
- nombre: {type:String, required: true},
- apellido: {type:String, required: true},
- dni: {type: String, required:true},
- email: {type: String, required: true}
-})
-module.exports = mongoose.models.Espectador || mongoose.model('Espectador', EspectadorSchema);
